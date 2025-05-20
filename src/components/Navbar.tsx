b584783749078318981439e6c94c873059604e38
andi-frame <andifarhan1094@gmail.com>
2025-05-20 17:01:53 +0700

    fix: navbar position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-rush-secondary flex justify-between items-center p-4">
+    <div className="w-full bg-rush-secondary flex justify-between items-center p-4 absolute top-0 z-10 shadow-md">
       <div className="text-2xl font-bold w-1/2"><a href="">RushPush</a></div>
       <div className="flex justify-between gap-10 pr-5">
         <a href="">Trakteer</a>
